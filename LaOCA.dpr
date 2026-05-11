c55ddcd4e0d8f215e11313927467cefa3c778d84
Arturo Renero <arl284682@gmail.com>
2026-05-10 20:24:27 -0600

    First Big Step
    Worked on setting up the file system for the project and found the characters for the game, created an example DB and started working on creating all the units we'll need for the whole development of the project

--- LaOCA.dpr
+++ LaOCA.dpr
@@ -3,7 +3,10 @@ program LaOCA;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  uMain in 'uMain.pas' {frmMain};
+  uMain in 'uMain.pas' {frmMain},
+  uTypes in 'src\uTypes.pas',
+  uDatabase in 'src\uDatabase.pas',
+  uBoardManager in 'src\uBoardManager.pas';
 
 {$R *.res}
 
---
+++ src/uBoardManager.pas
@@ -0,0 +1,7 @@
+unit uBoardManager;
+
+interface
+
+implementation
+
+end.
